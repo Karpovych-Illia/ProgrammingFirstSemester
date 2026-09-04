a256007b8b51122baf0618ccc4f40b68c5b83e57
Karpovych-Illia <l1ptsonnn@gmail.com>
2026-09-04 21:54:13 +0300

    (First commit): Main folder created, first lab task done

---
+++ Lab1/main.py
@@ -0,0 +1,36 @@
+
+print("Hello, World!")
+
+integer1 = int(1.33)
+
+list1 = [1,8%3,5//2,4-3,5]
+
+lenght1 = len(list1)
+
+float1 = 1.1255 * 7
+
+complex1 = 1 / 2j
+
+string1 = "Hello, World!"
+
+tuple1 = (1,2,3,4 ** 2,5)
+
+bool1 = True
+
+set1 = {1,2,3,4,5}
+
+if 12 >= 10:
+    task1 = 1 == 1
+    task2 = 1 != 1
+    task3 = 1 < 10
+    task4 = 10 > 1
+    task5 = 1 <= 10
+    task6 = 10 >= 1
+
+if bool1 == True:
+    range1 = range(1, 10)
+else:
+    range1 = range(10, 0)
+
+print(list1, integer1, float1, complex1, string1, tuple1, bool1, range1, set1)
+print('tasks:', task1, task2, task3, task4, task5, task6)
