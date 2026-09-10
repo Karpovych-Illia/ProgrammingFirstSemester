e7a45dbc7d59149aba53f8bb711fd49b3f630707
Karpovych-Illia <l1ptsonnn@gmail.com>
2026-09-10 22:43:23 +0300

    rewrite: updated code

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,4 +1,54 @@
 
 print("Hello, World!")
 
+intt = int(2)
+lenn = len('Hello, World')
+floatt = float(2.5)
+listt = ["a", "b", "c", "d"]
+print(listt)
+dictt = dict(a=1, b=2, c=3)
+strr = str('Hello, World')
+booll = bool(True)
+tuplee = (1, 2, 3)
+print(tuplee)
+sett = {1, 2, 3, 4}
+print(sett)
 
+a = 10
+b = 2
+print(a + b)
+
+a = 12
+b = 2
+print(a - b)
+
+a = 2
+b = 3
+print(a * b)
+
+a = 10
+b = 2
+print(a / b)
+
+a = 10
+b = 4
+print(a // b)
+
+a = 20
+b = 3
+print(a % b)
+
+a = 15
+b = 12
+print(a ** b)
+
+
+print(intt, type(intt))
+print(lenn, type(lenn))
+print(floatt, type(floatt))
+print(listt, type(listt))
+print(dictt, type(dictt))
+print(strr, type(strr))
+print(booll, type(booll))
+print(tuplee, type(tuplee))
+print(sett, type(sett))
